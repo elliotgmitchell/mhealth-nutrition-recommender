1febaf8ea7140f69635e52328e8b2343110dc242
Elliot Mitchell <elliotgmitchell@gmail.com>
2018-04-11 17:36:19 -0400

    update output format for nutritionix processing, and allow output csv to be incrementally updated

--- Data formatting/Data formatting.py
+++ Data formatting/Data formatting.py
@@ -3,15 +3,23 @@
 """
 Created on Mon Apr  9 14:54:34 2018
 
-@author: jeremylew
+@author: jeremylew, elliotgmitchell
 """
 
+import os
 import requests
 import json
 import pandas as pd
 
-app_id = "c0d12ddd"
-api_key = "f7e2c9f01df5c6372c4bc9290a2abe8d"
+app_id = ""
+api_key = ""
+
+# file path for raw data dump
+# data_path = "/Users/jeremylew/Dropbox (Personal)/mHealth Project/Data/data_dump_2.csv" # Jeremy's path
+data_path = os.path.expanduser("~/Dropbox/Columbia/Spring 2018/Data Science for Mobile Health/mHealth Project/Data/data_dump_2.csv") # Elliot's path
+
+# file path for processed output
+output_path = "./egm2143_processed_data.csv"
 
 # Version 2
 
@@ -26,45 +34,54 @@ def getFoodItems(user_id, meal_id, kind, query, user_foodID_table):
                                  data = json.dumps({
                                    "query":query,
                                  }))
-     
+
         foods_list=json.loads(response.text)["foods"]
         for food_dict in foods_list:
-            dataToBeInserted = {"user_id": user_id, 
+            dataToBeInserted = {"user_id": user_id,
                                 "meal_id": meal_id,
                                 "food": food_dict["food_name"],
+                                "carbs": food_dict["nf_total_carbohydrate"],
+                                "fat": food_dict["nf_total_fat"],
+                                "protein": food_dict["nf_protein"],
+                                "fiber": food_dict["nf_dietary_fiber"],
                                 "calories": food_dict["nf_calories"],
                                 "kind": kind}
             user_foodID_table = user_foodID_table.append(dataToBeInserted, ignore_index=True)
-        return user_foodID_table  
-         
+        return user_foodID_table
+
     except KeyError:
         print("Invalid query:",query)
-        return user_foodID_table    
-      
+        return user_foodID_table
+
 
 def createUserFoodTable(platanoData, user_foodID_table):
     for index,row in platanoData.iterrows():
-        try:  
+        try:
             food_query = str(row["title"]) + " " + str(row["ingredients"])
             user_foodID_table = getFoodItems(row["user_id"],row["meal_id"],row["kind"],food_query, user_foodID_table)
         except:
             print("Invalid query:", index, row["title"], row["ingredients"])
             continue
-        
+
     return user_foodID_table
-    
+
 
 # Read in Platano data
-platano = pd.read_csv("/Users/jeremylew/Dropbox (Personal)/mHealth Project/Data/data_dump_2.csv")
+platano = pd.read_csv(data_path)
 platano[["title","ingredients"]] = platano[["title","ingredients"]].fillna("")
 
 # Subset due to API limit:
-platano_subset = platano.loc[199:399,:]
+platano_subset = platano.loc[2:4,:]
 
 # Get user-food table
-user_foodID_table = pd.DataFrame(columns=["user_id","meal_id","food","calories","kind"]) #Creates empty data frame
+try:
+    user_foodID_table = pd.read_csv(output_path)
+    print("found existing output file")
+except FileNotFoundError as e:
+    user_foodID_table = pd.DataFrame(columns=["user_id","meal_id","food","carbs","protein","fat","fiber","calories","kind"]) #Creates empty data frame
+    print("starting output from scratch")
+
 user_foodID_table = createUserFoodTable(platano_subset,user_foodID_table) # Generate a user-food table
 
-# Export file 
-user_foodID_table.to_csv("/Users/jeremylew/Desktop/Row200-399.csv")
-
+# Export file
+user_foodID_table.to_csv(output_path, index=False)
